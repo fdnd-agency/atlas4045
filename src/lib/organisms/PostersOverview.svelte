ce195efcc34c0f53c5c1f17654dfb8c75fe835c1
Robin <robin.django@gmail.com>
2025-02-04 17:04:23 +0100

    feat: Added posters overview HTML skeleton ✨

---
+++ src/lib/organisms/PostersOverview.svelte
@@ -0,0 +1,15 @@
+<script>
+  import PosterCard from '../molecules/PosterCard.svelte';
+</script>
+
+<main>
+  <nav>
+    <h2>Overzicht</h2>
+    <a href="./">Kaart</a>
+  </nav>
+  <ul>
+    <!-- ForEach -->
+    <PosterCard />
+    <!-- /Foreach -->
+  </ul>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import PosterIntroductie from "../lib/organisms/PosterIntroductie.svelte";
+  import PostersOverview from "../lib/organisms/PostersOverview.svelte";
 </script>
 
 <PosterIntroductie />
+<PostersOverview />
