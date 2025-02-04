5dca57b8fec9d65ee0715152055c440cfaf2c4b0
Robin <robin.django@gmail.com>
2025-02-04 17:03:39 +0100

    feat: Added header HTML skeleton ✨

---
+++ src/lib/organisms/Header.svelte
@@ -0,0 +1,6 @@
+<header>
+  <img src="/assets/logo.avif" alt="4 Mei Comité Oosterparkbuurt" width="404" height="178">
+  <a href="mailto:4meioosterparkbuurt@gmail.com">Contact</a>
+  <a href="mailto:4meioosterparkbuurt@gmail.com?subject=Ik wil me abonneren op de nieuwsbrief">Nieuwsbrief</a>
+  <h1>Gedenkposters</h1>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Header from "../lib/organisms/Header.svelte";
+
+  let { children } = $props();
+</script>
+
+<Header />
+
+{@render children()}
