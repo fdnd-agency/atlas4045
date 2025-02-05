9d536bc108910a3724d621a8edf9a5aafdacd89b
Robin <robin.django@gmail.com>
2025-02-05 14:48:18 +0100

    feat: Added reusable Nav and NavItem components for navigation
    Added Nav component which takes NavItems as children and renders them in a consistent navigation style.
    
    NavItem can take two arguments:
    
    - active<boolean>: denotes whether this is the page/tab that the navigation is on. Currently this renders as an h2 as this is presumably also the title of said page/tab.
    - href<string>: the path that the navigation item should link to when clicked. Does nothing if active is set to true.
    
    NavItem renders as a span if no href is given.

---
+++ src/lib/molecules/Nav.svelte
@@ -0,0 +1,23 @@
+<script>
+  let { children } = $props();
+</script>
+
+<nav>
+  <ul>
+    {@render children()}
+  </ul>
+</nav>
+
+<style>
+  nav {
+    padding: 1rem 4rem;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
