7a98818d32c44a3dac287fee5fb665b671860deb
Robin <robin.django@gmail.com>
2025-02-06 15:01:18 +0100

    feat: Added hover & focus styles on LinkButton :lipstick:

--- src/lib/atoms/LinkButton.svelte
+++ src/lib/atoms/LinkButton.svelte
@@ -16,8 +16,19 @@
     height: fit-content;
     text-decoration: none;
     color: var(--color-brown-dark);
-    font-weight: bold;
+    font-weight: var(--font-weight-bold);
     width: fit-content;
+
+    transition-property: border, background-color, color;
+    transition-duration: 0.15s;
+    transition-timing-function: ease-in-out;
+
+    &:hover, 
+    &:focus {
+      background-color: var(--color-brown);
+      color: var(--color-white);
+      border: 2px solid var(--color-brown);
+    }
   }
 
   .full-width {
