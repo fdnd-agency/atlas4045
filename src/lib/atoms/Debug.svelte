512ed44e03e080c594436339e87081daf01a4e57
Robin <robin.django@gmail.com>
2025-02-06 15:39:27 +0100

    refactor: Cleaned up dead code from Debug component ⚰️

--- src/lib/atoms/Debug.svelte
+++ src/lib/atoms/Debug.svelte
@@ -1,9 +1,6 @@
 <script>
 	let width = $state(0);
 	let height = $state(0);
-
-	let randomColor1 = Math.random() * (135 - 60) + 60;
-	let randomColor2 = Math.random() * (315 - 220) + 220;
 </script>
 
 <div
@@ -18,7 +15,6 @@
 	div {
     background: linear-gradient(150deg, var(--color-brown-light) 0%, var(--color-brown) 100%);
     border: 1px var(--color-brown-dark) solid;
-		display: inline-block;
 		height: 100%;
 		width: 100%;
 		overflow: hidden;
