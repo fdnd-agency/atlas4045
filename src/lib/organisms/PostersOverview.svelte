9d6abc44a8238b5adf3ca8c8229d95a0562a5527
Robin <robin.django@gmail.com>
2025-02-06 11:39:44 +0100

    feat: Added first iteration of cards overview styling :lipstick:

--- src/lib/organisms/PostersOverview.svelte
+++ src/lib/organisms/PostersOverview.svelte
@@ -5,10 +5,18 @@
 </script>
 
 <ul>
-  <!-- ForEach -->
-  <PosterCard />
-  <!-- /Foreach -->
+  {#each posters as poster}
+    <PosterCard name={poster.name} surname={poster.surname} address={poster.address} />
+  {/each}
 </ul>
 
 <style>
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 1rem;
+    padding-top: 3rem;
+  }
+
 </style>
