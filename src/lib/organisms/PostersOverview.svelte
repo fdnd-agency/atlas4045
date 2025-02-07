22f2442fca7cc288df496e4466d27b616598b1ee
Robin <robin.django@gmail.com>
2025-02-07 13:48:31 +0100

    Feat: Added some fallback values for if there is no data in PosterCard

--- src/lib/organisms/PostersOverview.svelte
+++ src/lib/organisms/PostersOverview.svelte
@@ -7,10 +7,10 @@
 <ul>
 	{#each posters as posterData}
 		<PosterCard
-			name={posterData.family.family_name}
-			street={posterData.street}
-			house_number={posterData.house_number}
-			image={posterData.poster.covers[1].directus_files_id}
+			name={posterData.family.family_name ?? "Onbekend"}
+			street={posterData.street ?? "Onbekend"}
+			house_number={posterData.house_number ?? "Onbekend"}
+			image={posterData.poster.covers[1].directus_files_id ?? ""}
 		/>
 	{/each}
 </ul>
