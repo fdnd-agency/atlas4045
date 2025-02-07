409d262010f63aaef554ba7358142e57d33d44e4
Robin <robin.django@gmail.com>
2025-02-07 10:57:44 +0100

    feat: Set up Directus SDK :sparkles:

---
+++ src/routes/+page.js
@@ -0,0 +1,8 @@
+import getDirectusInstance from '$lib/directus';
+import { readItems } from '@directus/sdk';
+export async function load({ fetch }) {
+	const directus = getDirectusInstance(fetch);
+	return {
+		adress: await directus.request(readItems('atlas_address')),
+	};
+}
