aef2a7ee1e8c95a848bed36516a6ccb712cd0844
Robin <robin.django@gmail.com>
2025-02-10 11:29:02 +0100

    feat: Created custom Image component which automatically optimises images :sparkles:

---
+++ src/lib/atoms/Image.svelte
@@ -0,0 +1,34 @@
+<!-- This component should be used when rendering an image from the Directus backend to automatically optimise it -->
+
+<script>
+	let { src = '', alt = 'Placeholder', width = 400, height = 300 } = $props();
+  
+</script>
+
+{#if src === ''}
+	<img
+		src="https://placehold.co/{width}x{height}/c29f9d/3e2518?text=No+image+found"
+		alt="Placeholder"
+		style="--img-width: {width}; --img-height: {height};"
+	/>
+{:else}
+	<picture>
+		<source srcset="{src}?fit=cover&width={width}&height={height}&format=avif" />
+		<source srcset="{src}?fit=cover&width={width}&height={height}&format=webp" />
+		<img
+			src="{src}?fit=cover&width={width}&height={height}&format=png"
+			{width}
+			{height}
+			{alt}
+			style="--img-width: {width}; --img-height: {height}"
+		/>
+	</picture>
+{/if}
+
+<style>
+	img {
+		width: 100%;
+		height: auto;
+		aspect-ratio: var(--img-width) / var(--img-height);
+	}
+</style>
