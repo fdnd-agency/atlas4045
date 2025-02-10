1848fad39ab901f032020802515f770023183ce5
Robin <robin.django@gmail.com>
2025-02-10 11:42:50 +0100

    refactor: Renamed Image component to DirectusImage :recycle:

--- src/lib/atoms/DirectusImage.svelte
+++ src/lib/atoms/DirectusImage.svelte
@@ -2,15 +2,17 @@
 
 <script>
 	let { 
-    src = "", 
+    imageId = "", 
     alt = "Placeholder", 
     width = 400, 
     height = 300, 
     loading = "eager", 
   } = $props();
+
+  const URL = "https://fdnd-agency.directus.app/assets/";
 </script>
 
-{#if src === ''}
+{#if imageId === ""}
 <!-- Use placehold.co API to generate a custom placeholder image -->
 	<img
 		src="https://placehold.co/{width}x{height}/c29f9d/3e2518?text=No+image+found"
@@ -23,10 +25,10 @@
 {:else}
 <!-- Use <picture> to optimise image format using Progressive Enhancement -->
 	<picture>
-		<source srcset="{src}?fit=cover&width={width}&height={height}&format=avif" />
-		<source srcset="{src}?fit=cover&width={width}&height={height}&format=webp" />
+		<source srcset="{URL}{imageId}?fit=cover&width={width}&height={height}&format=avif" />
+		<source srcset="{URL}{imageId}?fit=cover&width={width}&height={height}&format=webp" />
 		<img
-			src="{src}?fit=cover&width={width}&height={height}&format=png"
+			src="{URL}{imageId}?fit=cover&width={width}&height={height}&format=png"
 			{width}
 			{height}
 			{alt}
