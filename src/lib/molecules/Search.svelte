2cc38f723d418115b2bf10278ba0e0f38668d258
Robin <robin.django@gmail.com>
2025-02-12 16:22:45 +0100

    feat: Added Search widget :sparkles:

---
+++ src/lib/molecules/Search.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Button from '$lib/atoms/Button.svelte';
+	import TextInput from '$lib/atoms/TextInput.svelte';
+</script>
+
+<div>
+  <TextInput type="text" placeholder="Zoeken..." sronly="true" id="search" />
+	<Button type="submit">Zoeken</Button>
+</div>
+
+<style>
+	div {
+		display: flex;
+    align-items: center;
+    justify-content: center;
+		gap: var(--spacing-xs);
+	}
+</style>
