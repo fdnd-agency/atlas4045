15e34ccc29b4de6fe17f5c710fd3483c3535d4b6
Robin <robin.django@gmail.com>
2025-02-12 16:23:08 +0100

    feat: Added PostersFilter component (incomplete) :sparkles:

---
+++ src/lib/molecules/PostersFilter.svelte
@@ -0,0 +1,19 @@
+<script>
+  import TextInput from '$lib/atoms/TextInput.svelte';
+	import Search from '$lib/molecules/Search.svelte';
+</script>
+
+<form>
+  <TextInput id="birth-date" type="number" placeholder="Geboortedatum"/>
+
+	<Search />
+</form>
+
+<style>
+	form {
+		display: flex;
+    align-items: center;
+    justify-content: center;
+		gap: 1rem;
+	}
+</style>
