2da8e0b8abefa2252480ea104d7d1971a13d6248
Robin <robin.django@gmail.com>
2025-02-12 14:13:49 +0100

    feat: Added ability for Button to render <button> OR <a> :sparkles:

--- src/lib/atoms/Button.svelte
+++ src/lib/atoms/Button.svelte
@@ -1,33 +1,43 @@
 <script>
-  let { children, href, class: classList = "" } = $props();
+	let { children, href = null,  onclick = null, class: classList = '' } = $props();
 </script>
 
-<a {href} class="no-focus {classList}">
-  {@render children()}
-</a>
+{#if href}
+	<a {href} class="no-focus {classList}">
+		{@render children()}
+	</a>
+{:else if onclick}
+	<button {onclick} class="no-focus {classList}">
+		{@render children()}
+	</button>
+{:else}
+  <button class="no-focus {classList}">
+    {@render children()}
+  </button>
+{/if}
 
 <style>
-  a {
-    display: inline-block;
-    width: fit-content;
-    height: fit-content;
-    padding: var(--spacing-xxs) var(--spacing-sm);
-    border-radius: var(--border-radius-pill);
-    border: 2px solid var(--brown-dark);
-    text-align: center;
-    text-decoration: none;
-    font-weight: var(--font-weight-bold);
-    color: var(--brown-dark);
+	a {
+		display: inline-block;
+		width: fit-content;
+		height: fit-content;
+		padding: var(--spacing-xxs) var(--spacing-sm);
+		border-radius: var(--border-radius-pill);
+		border: 2px solid var(--brown-dark);
+		text-align: center;
+		text-decoration: none;
+		font-weight: var(--font-weight-bold);
+		color: var(--brown-dark);
 
-    transition-property: border, background-color, color;
-    transition-duration: 0.15s;
-    transition-timing-function: ease-in-out;
+		transition-property: border, background-color, color;
+		transition-duration: 0.15s;
+		transition-timing-function: ease-in-out;
 
-    &:hover, 
-    &:focus-visible {
-      background-color: var(--brown-neutral);
-      color: var(--white);
-      border: 2px solid var(--brown-neutral);
-    }
-  }
+		&:hover,
+		&:focus-visible {
+			background-color: var(--brown-neutral);
+			color: var(--white);
+			border: 2px solid var(--brown-neutral);
+		}
+	}
 </style>
