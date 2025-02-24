009d8c6075f13e8f6552db8f16711e70b92143be
Robin <robin.django@gmail.com>
2025-02-24 15:09:23 +0100

    feat: Added rest properties to TextDropdownInput :sparkles:

--- src/lib/atoms/TextDropdownInput.svelte
+++ src/lib/atoms/TextDropdownInput.svelte
@@ -9,11 +9,12 @@
 		children,
 		inputClass = '',
 		labelClass = '',
-		list = ['Option 1', 'Option 2', 'Option 3']
+		list = ['Option 1', 'Option 2', 'Option 3'],
+    ...rest
 	} = $props();
 </script>
 
-<TextInput type="text" {placeholder} {id} list={`${id}-list`} {inputClass} {labelClass}>{@render children()}</TextInput>
+<TextInput type="text" {placeholder} {id} list={`${id}-list`} {inputClass} {labelClass} {...rest}>{@render children()}</TextInput>
 <datalist id={`${id}-list`}>
 	{#each list as option}
 		<option>{option}</option>
