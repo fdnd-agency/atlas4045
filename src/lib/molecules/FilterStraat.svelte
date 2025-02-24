85dbacd63718c3d51dade3a5d7c9cb904069f18a
Robin <robin.django@gmail.com>
2025-02-24 14:23:41 +0100

    feat: Added FilterStraat component :sparkles:

---
+++ src/lib/molecules/FilterStraat.svelte
@@ -0,0 +1,17 @@
+<script>
+	import FilterCard from '$lib/atoms/FilterCard.svelte';
+	import TextDropdownInput from '$lib/atoms/TextDropdownInput.svelte';
+	import Checkbox from '$lib/atoms/Checkbox.svelte';
+
+  let { onchange } = $props();
+</script>
+
+<FilterCard>
+	<TextDropdownInput id="straat" placeholder="Straat" labelClass={$css('label')}>Straatnaam</TextDropdownInput>
+</FilterCard>
+
+<style>
+  .label {
+    margin: auto;
+  }
+</style>
