00b447a776498070f1e533f1c627009e5a3b11fb
Robin <robin.django@gmail.com>
2025-02-24 15:10:21 +0100

    feat: Bubbled onchange event up from TextDropdownInput to PostersFilter ✨

--- src/lib/molecules/FilterStraat.svelte
+++ src/lib/molecules/FilterStraat.svelte
@@ -2,16 +2,35 @@
 	import FilterCard from '$lib/atoms/FilterCard.svelte';
 	import TextDropdownInput from '$lib/atoms/TextDropdownInput.svelte';
 	import Checkbox from '$lib/atoms/Checkbox.svelte';
+	import { page } from '$app/state';
 
-  let { onchange } = $props();
+	let { onchange } = $props();
+
+	let streetsList = $derived(
+		Array.from(
+			// Remove duplicates
+			new Set(
+				// Get all streets from posters
+				page.data.posters.map((poster) => poster.street.trim())
+			)
+		).sort() // Sort alphabetically
+	);
 </script>
 
 <FilterCard>
-	<TextDropdownInput id="straat" placeholder="Straat" labelClass={$css('label')}>Straatnaam</TextDropdownInput>
+	<TextDropdownInput
+		id="straat"
+		placeholder="Straat"
+		labelClass={$css('label')}
+		list={streetsList}
+    {onchange}
+	>
+		Straatnaam
+	</TextDropdownInput>
 </FilterCard>
 
 <style>
-  .label {
-    margin: auto;
-  }
+	.label {
+		margin: auto;
+	}
 </style>
