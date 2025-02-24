f1cb37ad0df1cca6bb156122aa1b8fea9c55e5f8
Robin <robin.django@gmail.com>
2025-02-24 13:53:40 +0100

    feat: Added a reusable TextDropdownInput element ✨
    Added a reusable element for creating a text input with a dropdown list attached to it, using the HTML datalist element.

---
+++ src/lib/atoms/TextDropdownInput.svelte
@@ -0,0 +1,19 @@
+<script>
+  let { 
+    placeholder = "Input", 
+    sronly = false, 
+    id, 
+    type = "text", 
+    children, 
+    inputClass = "", 
+    labelClass = "",
+    list = ["Option 1", "Option 2", "Option 3"],
+  } = $props();
+</script>
+
+<TextInput type="text" placeholder="Straat" id="straat" list={`${id}-list`}>Straatnaam</TextInput>
+<datalist id={`${id}-list`}>
+  {#each list as option}
+    <option>{option}</option>
+  {/each}
+</datalist>
