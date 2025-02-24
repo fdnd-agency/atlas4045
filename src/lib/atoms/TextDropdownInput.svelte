bb0750303f431f3b631f309a235850be8a66d5ab
Robin <robin.django@gmail.com>
2025-02-24 14:18:33 +0100

    fix: Fixed TextDropdownInput not working :bug:

--- src/lib/atoms/TextDropdownInput.svelte
+++ src/lib/atoms/TextDropdownInput.svelte
@@ -1,19 +1,21 @@
 <script>
-  let { 
-    placeholder = "Input", 
-    sronly = false, 
-    id, 
-    type = "text", 
-    children, 
-    inputClass = "", 
-    labelClass = "",
-    list = ["Option 1", "Option 2", "Option 3"],
-  } = $props();
+	import TextInput from '$lib/atoms/TextInput.svelte';
+
+	let {
+		placeholder = 'Input',
+		sronly = false,
+		id,
+		type = 'text',
+		children,
+		inputClass = '',
+		labelClass = '',
+		list = ['Option 1', 'Option 2', 'Option 3']
+	} = $props();
 </script>
 
-<TextInput type="text" placeholder="Straat" id="straat" list={`${id}-list`}>Straatnaam</TextInput>
+<TextInput type="text" {placeholder} {id} list={`${id}-list`}>{@render children()} {inputClass} {labelClass}</TextInput>
 <datalist id={`${id}-list`}>
-  {#each list as option}
-    <option>{option}</option>
-  {/each}
+	{#each list as option}
+		<option>{option}</option>
+	{/each}
 </datalist>
