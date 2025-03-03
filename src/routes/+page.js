191e74f99e0670f43bc4a8ba1e0f80f52d079f96
Robin <robin.django@gmail.com>
2025-03-03 12:47:36 +0100

    refactor: Changed +page.server.js to +page.js 🏗️
    Changed +page.server.js to +page.js, so that data can also be fetched through the client when navigation is handled clientside by Svelte. This means that the URL can be used to filter through the posters when the search function is used, by simply letting Svelte call the load function on the client.

---
+++ src/routes/+page.js
@@ -0,0 +1,50 @@
+import getDirectusInstance from '$lib/directus';
+import { readItems } from '@directus/sdk';
+
+export async function load({ fetch, url }) {
+  let filters = {};
+  console.log("load function called");
+  
+  if (url.searchParams.get('straat')) {
+    filters.street = {
+      _icontains: url.searchParams.get('straat')
+    }
+  }
+	
+	try {
+		const directus = getDirectusInstance(fetch);
+		return {
+			posters: await directus.request(
+				readItems('atlas_address', {
+					filter: filters,
+					fields: [
+						'id',
+						'street',
+						'house_number',
+						'floor',
+						'addition',
+						{
+							person: ['first_name', 'last_name']
+						},
+						{
+							poster: [
+								'id',
+								{
+									covers: [
+										'directus_files_id.id',
+										'directus_files_id.width',
+										'directus_files_id.height'
+									]
+								}
+							]
+						}
+					],
+					sort: ['street', 'house_number', 'addition', 'floor']
+				})
+			)
+		};
+	} catch (error) {
+		console.error(error);
+		return {}; // Return empty object if error
+	}
+}
