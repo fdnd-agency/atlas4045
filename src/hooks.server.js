dcba3143732a6ce677d59d58364638d874ba9060
Robin <robin.django@gmail.com>
2025-03-03 12:48:34 +0100

    fix: Fixed hooks.server.js no longer serializing headers 🐛

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,7 +3,11 @@ export async function handle({ event, resolve }) {
 
 	// Allow access to the login page
 	if (event.url.pathname.startsWith("/login")) {
-			return resolve(event)
+			return resolve(event, {
+        filterSerializedResponseHeaders: (key) => {
+          return key.toLowerCase() === 'content-type';
+        },
+      })
 	}
 
 	// If not authenticated, redirect to login
@@ -11,5 +15,9 @@ export async function handle({ event, resolve }) {
 			return new Response("Redirecting...", { status: 303, headers: { location: "/login" } })
 	}
 
-	return resolve(event)
+	return resolve(event, {
+		filterSerializedResponseHeaders: (key) => {
+			return key.toLowerCase() === 'content-type';
+		},
+	})
 }
