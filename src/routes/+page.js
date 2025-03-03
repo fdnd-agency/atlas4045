4c8ed44135277ede29f6b7f0ed27cdb761356f2f
Robin <robin.django@gmail.com>
2025-03-03 15:04:35 +0100

    refactor: Refactored the query fields into const variable ♻️

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,42 +2,40 @@ import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
 
 export async function load({ fetch, url }) {
-  let filters = {};
+  let queryFilters = {};
   
   if (url.searchParams.get('straat')) {
-    filters.street = {
+    queryFilters.street = {
       _icontains: url.searchParams.get('straat')
     }
   }
+
+  const queryFields = [
+    'id',
+    'street',
+    'house_number',
+    'floor',
+    'addition',
+    { person: ['first_name', 'last_name'] },
+    { poster: [
+        'id',
+        { covers: [
+            'directus_files_id.id',
+            'directus_files_id.width',
+            'directus_files_id.height'
+          ]
+        }
+      ]
+    }
+  ];
 	
 	try {
 		const directus = getDirectusInstance(fetch);
 		return {
 			posters: await directus.request(
 				readItems('atlas_address', {
-					filter: filters,
-					fields: [
-						'id',
-						'street',
-						'house_number',
-						'floor',
-						'addition',
-						{
-							person: ['first_name', 'last_name']
-						},
-						{
-							poster: [
-								'id',
-								{
-									covers: [
-										'directus_files_id.id',
-										'directus_files_id.width',
-										'directus_files_id.height'
-									]
-								}
-							]
-						}
-					],
+					filter: queryFilters,
+					fields: queryFields,
 					sort: ['street', 'house_number', 'addition', 'floor']
 				})
 			)
