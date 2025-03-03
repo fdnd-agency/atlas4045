b45d9a3e8c84f330fed2f201d72d2c72d94c4486
Robin <robin.django@gmail.com>
2025-03-03 12:49:40 +0100

    feat: Added suspense boundary to PostersOverview ✨

--- src/lib/organisms/PostersOverview.svelte
+++ src/lib/organisms/PostersOverview.svelte
@@ -1,31 +1,39 @@
 <script>
-  import PosterCard from "../molecules/PosterCard.svelte";
-  import PostersFilter from "$lib/organisms/PostersFilter.svelte";
+	import PosterCard from '../molecules/PosterCard.svelte';
+	import PostersFilter from '$lib/organisms/PostersFilter.svelte';
 
-	let { posters = [] } = $props()
+	let { posters = [] } = $props();
 </script>
-
 
 <PostersFilter />
 
-{#if posters.length > 0}
-	<ul>
-		{#each posters as posterData}
-			<PosterCard
-				name={posterData.person?.[0]?.last_name ?? "Onbekend"}
-				street={posterData.street ?? "Onbekend"}
-				house_number={posterData.house_number ?? "Onbekend"}
-				floor={posterData?.floor}
-				addition={posterData?.addition}
-				image={posterData.poster?.covers?.[0]?.directus_files_id?.id ?? ""}
-				width={posterData.poster?.covers?.[0]?.directus_files_id?.width ?? 419}
-				height={posterData.poster?.covers?.[0]?.directus_files_id?.height ?? 585}
-			/>
-		{/each}
-	</ul>
-{:else}
-	<p>Er zijn momenteel geen posters beschikbaar. Kom later terug of neem contact met ons op via de knop bovenin de pagina als dit probleem aanhoudt.</p>
-{/if}
+{#await posters}
+	Loading posters...
+{:then posters}
+	{#if posters.length > 0}
+		<ul>
+			{#each posters as posterData}
+				<PosterCard
+					name={posterData.person?.[0]?.last_name ?? 'Onbekend'}
+					street={posterData.street ?? 'Onbekend'}
+					house_number={posterData.house_number ?? 'Onbekend'}
+					floor={posterData?.floor}
+					addition={posterData?.addition}
+					image={posterData.poster?.covers?.[0]?.directus_files_id?.id ?? ''}
+					width={posterData.poster?.covers?.[0]?.directus_files_id?.width ?? 419}
+					height={posterData.poster?.covers?.[0]?.directus_files_id?.height ?? 585}
+				/>
+			{/each}
+		</ul>
+	{:else}
+		<p>
+			Er zijn momenteel geen posters beschikbaar. Kom later terug of neem contact met ons op via de
+			knop bovenin de pagina als dit probleem aanhoudt.
+		</p>
+	{/if}
+{:catch error}
+	<p>error loading posters: {error.message}</p>
+{/await}
 
 <style>
 	ul {
