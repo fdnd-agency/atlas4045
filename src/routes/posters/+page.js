00110c4792b389286d30f3009dcd42afe2495e83
Robin <robin.django@gmail.com>
2025-03-05 15:17:33 +0100

    refactor: Moved overview page to /posters 🚚

---
+++ src/routes/posters/+page.js
@@ -0,0 +1,47 @@
+import getDirectusInstance from '$lib/directus';
+import { readItems } from '@directus/sdk';
+
+export async function load({ fetch, url }) {
+  let queryFilters = {};
+  
+  if (url.searchParams.get('straat')) {
+    queryFilters.street = {
+      _icontains: url.searchParams.get('straat')
+    }
+  }
+
+  const queryFields = [
+    'id',
+    'street',
+    'house_number',
+    'floor',
+    'addition',
+    { person: ['first_name', 'last_name'] },
+    { poster: [
+        'id',
+        { covers: [
+            'directus_files_id.id',
+            'directus_files_id.width',
+            'directus_files_id.height'
+          ]
+        }
+      ]
+    }
+  ];
+	
+	try {
+		const directus = getDirectusInstance(fetch);
+		return {
+			posters: await directus.request(
+				readItems('atlas_address', {
+					filter: queryFilters,
+					fields: queryFields,
+					sort: ['street', 'house_number', 'addition', 'floor']
+				})
+			)
+		};
+	} catch (error) {
+		console.error(error);
+		return {}; // Return empty object if error
+	}
+}
---
+++ src/routes/posters/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import PosterIntroductie from "$lib/organisms/PosterIntroductie.svelte"
+
+  import Nav from "$lib/molecules/Nav.svelte"
+  import NavItem from "$lib/atoms/NavItem.svelte"
+  import PostersOverview from "$lib/organisms/PostersOverview.svelte"
+
+  let { data } = $props();
+</script>
+
+<!-- Overview section -->
+<main>
+  <PostersOverview posters={data.posters} />
+</main>
+
+<PosterIntroductie />
+
+<style>
+  main {
+    max-width: var(--max-width-mobile);
+    margin: 0 auto;
+  }
+
+  @media screen and (min-width: 800px) {
+    main {
+      max-width: var(--max-width-desktop);
+    }
+  }
+</style>
