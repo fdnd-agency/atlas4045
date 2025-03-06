50e0742487716be946c88337ed51e3421d0f7840
Robin <robin.django@gmail.com>
2025-03-06 14:08:55 +0100

    feat: Improved FilterCard styles for new design 💄

--- src/lib/atoms/FilterCard.svelte
+++ src/lib/atoms/FilterCard.svelte
@@ -11,20 +11,21 @@
     width: fit-content;
     padding-left: var(--spacing-sm);
     display: flex;
+    align-items: center;
     gap: var(--spacing-xs);
     background-color: var(--brown-100);
     border-radius: var(--border-radius-pill);
 
-    transition-property: border, background-color;
+    transition-property: background-color, color;
 		transition-duration: 0.15s;
 		transition-timing-function: ease-in-out;
 
-    &:focus-within {
+    &:global(:has(input:not(:placeholder-shown))) {
       background-color: var(--brown-300);
     }
-
-    &:has( input:not(:placeholder-shown)) {
-      background-color: var(--brown-300);
+    &:focus-within {
+      color: white;
+      background-color: var(--brown-600) !important;
     }
   }
 </style>
