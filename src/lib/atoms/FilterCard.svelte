dbed77359c6a987bfea517dd37c933c830292823
Robin <robin.django@gmail.com>
2025-03-06 13:01:06 +0100

    refactor: Redesigned FilterCard to be more compact ✨

--- src/lib/atoms/FilterCard.svelte
+++ src/lib/atoms/FilterCard.svelte
@@ -7,16 +7,24 @@
 </div>
 
 <style>
-	div {
+  div {
     width: fit-content;
-    padding: var(--spacing-xs);
-		display: flex;
-    flex-direction: column;
-		gap: var(--spacing-xs);
+    padding-left: var(--spacing-sm);
+    display: flex;
+    gap: var(--spacing-xs);
+    background-color: var(--brown-100);
+    border-radius: var(--border-radius-pill);
 
-    background-color: var(--brown-dark); 
-    color: var(--white);
-    /* border: 2px solid var(--brown-dark); */
-    border-radius: var(--spacing-sm);
-	}
+    transition-property: border, background-color;
+		transition-duration: 0.15s;
+		transition-timing-function: ease-in-out;
+
+    &:focus-within {
+      background-color: var(--brown-300);
+    }
+
+    &:has( input:not(:placeholder-shown)) {
+      background-color: var(--brown-300);
+    }
+  }
 </style>
