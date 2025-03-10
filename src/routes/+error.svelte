57551b276a71bc66fa67083d8503cf127b11a654
Robin <robin.django@gmail.com>
2025-03-10 11:17:24 +0100

    feat: Added simple error page 🚸

---
+++ src/routes/+error.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<main>
+  <h2>Er is iets fout gegaan.</h2>
+  <p>Deze pagina bestaat niet of is tijdelijk niet beschikbaar.</p>
+  <p class="error-message">{$page.status}: {$page.error.message}</p>
+</main>
+
+<style>
+  main {
+    max-width: var(--max-width-mobile);
+    margin: 0 auto;
+    padding: var(--spacing-sm) var(--page-padding-mobile);
+  }
+
+h2 {
+  font-size: var(--font-size-xxl);
+  font-weight: var(--font-weight-bold);
+  color: var(--neutral-900);
+  margin-bottom: var(--spacing-md);
+  width: 80%;
+  margin: auto;
+  padding-bottom: var(--spacing-md);
+}
+
+p {
+  font-size: var(--font-size-md);
+  color: var(--neutral-900);
+  text-align: justify;
+  margin: auto;
+  margin-bottom: var(--spacing-md);
+  width: 80%;
+}
+
+.error-message {
+  font-size: var(--font-size-md);
+  color: var(--brown-600);
+}
+
+@media screen and (min-width: 800px) {
+  main {
+    max-width: var(--max-width-desktop);
+    padding: var(--spacing-sm) var(--page-padding-desktop);
+  }
+}
+</style>
