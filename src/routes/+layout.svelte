4eeb88add924bd36b37eb90a45cabc4c53baed39
Robin <robin.django@gmail.com>
2025-04-01 12:43:06 +0200

    refactor: Moved javascript check logic to util import ♻️

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,16 @@
   import Logout from "$lib/molecules/Logout.svelte"
   import Header from "$lib/organisms/Header.svelte"
   import ColourTester from "$lib/utils/ColourTester.svelte"
+  import { javascript } from "$lib/utils/javascriptEnabled.svelte.js";
+  import { onMount } from "svelte";
 
   let { children, data } = $props()
   let { isAuthenticated } = data
+
+    // Check if javascript is enabled using onMount so that we only hide the submit button if JS is enabled
+	onMount(() => {
+		javascript.enabled = true;
+	});
 </script>
 
 <!-- Devtool. Remove before actual site goes live -->
