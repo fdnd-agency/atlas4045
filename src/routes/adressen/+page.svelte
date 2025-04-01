85c453966af140d149b657cc7a5c84a1601626a7
Robin <robin.django@gmail.com>
2025-04-01 13:56:39 +0200

    feat: Added SideFilter to +page ✨

--- src/routes/adressen/+page.svelte
+++ src/routes/adressen/+page.svelte
@@ -1,18 +1,42 @@
 <script>
 	import PostersOverview from '$lib/organisms/PostersOverview.svelte';
 	import Map from '$lib/organisms/Map.svelte';
+	import SideFilter from '$lib/organisms/SideFilter.svelte';
 
 	let { data } = $props();
 	let mapAddresses = $derived(data.addresses.filter((address) => address.map?.coordinates));
+	let filterOpen = $state(false);
 </script>
 
-<main>
+<main class="posters-overview">
 	<Map {mapAddresses} />
+	<SideFilter bind:filterOpen />
+	<PostersOverview addresses={data.addresses} {filterOpen} />
 </main>
 
 <style>
-  main {
-    margin: 0 auto;
-    padding: var(--spacing-sm) var(--page-padding);
-  }
+	main {
+		margin: 0 auto;
+		display: grid;
+		grid-template-areas:
+			'map map'
+      'filter header'
+			'filter posters';
+	}
+
+	:global(main.posters-overview > section) {
+		grid-area: map; 
+	}
+
+	:global(main.posters-overview > aside) {
+		grid-area: filter;
+	}
+
+	:global(main.posters-overview > header) {
+		grid-area: header;
+	}
+
+	:global(main.posters-overview > ul) {
+		grid-area: posters;
+	}
 </style>
