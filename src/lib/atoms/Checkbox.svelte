8ae1f6ece33bb98317f46bba30efea55326ac242
Robin <robin.django@gmail.com>
2025-04-04 10:27:11 +0200

    feat: Added rest props to Checkbox ✨

--- src/lib/atoms/Checkbox.svelte
+++ src/lib/atoms/Checkbox.svelte
@@ -8,10 +8,11 @@
 		checked = '',
 		value = 'on',
     onchange,
+    ...rest
 	} = $props();
 </script>
 	<!-- Give label a sr-only class if (sronly) is truthy -->
-	<input {id} type="checkbox" class="no-focus {inputClass}" {onchange} />
+	<input {id} type="checkbox" class="no-focus {inputClass}" {onchange}  {...rest} />
 	<label for={id} class={[sronly && 'sr-only', labelClass]}>{@render children()}</label>
 
 <style>
