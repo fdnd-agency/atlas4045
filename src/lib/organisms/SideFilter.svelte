bb9e89c4aaea0e18a09c3c31fd6d1d8db6e48b01
Robin <robin.django@gmail.com>
2025-04-08 16:10:26 +0200

    refactor: Tweaked breakpoint for filter hiding 📱

--- src/lib/organisms/SideFilter.svelte
+++ src/lib/organisms/SideFilter.svelte
@@ -148,7 +148,7 @@
 		border-width: 0;
 	}
 
-	@media screen and (min-width: 460px) {
+	@media screen and (min-width: 800px) {
 		aside {
 			position: sticky;
 			box-shadow: -20px 0px 10px 20px rgba(0, 0, 0, 0.303);
