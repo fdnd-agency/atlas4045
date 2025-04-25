b0155b5d6bcdab1b9f2619a2e3bc3c4f5ddbfc88
Robin <robin.django@gmail.com>
2025-04-25 12:45:58 +0200

    feat: Removed login/logout routes :fire:

--- src/routes/logout/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@sveltejs/kit"
-
-export const actions = {
-    default: async ({ cookies }) => {
-        cookies.delete("auth_token", { path: "/" })
-
-        throw redirect(303, "/login")
-    }
-};
