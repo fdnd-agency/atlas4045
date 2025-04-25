9a55ec3cd89aac9a1c1cefc18ebc23bd7ec81799
Robin <robin.django@gmail.com>
2025-04-25 12:18:23 +0200

    feat: Made PostersTitle dynamic based on filter 🚸

--- src/lib/atoms/PostersTitle.svelte
+++ src/lib/atoms/PostersTitle.svelte
@@ -3,22 +3,16 @@
 
   let { length } = $props();
 
-	let filterMessage = 'Adressen met een gedenkposter gevonden met';
-  let name = $derived(page.url.searchParams.get('naam'));
-  let street = $derived(page.url.searchParams.get('straat'));
-
+  let streets = $derived(page.url.searchParams.getAll('s'));
 </script>
 
 <h2>
-{#if name && street}
-	{filterMessage} <strong>naam <span class="highlight">{name}</span></strong> <br>en <strong>straat
-	<span class="highlight">{street}</span></strong>
-{:else if name}
-	{filterMessage} <strong>naam <span class="highlight">{name}</span></strong>
-{:else if street}
-	{filterMessage} <strong>straat <span class="highlight">{street}</span></strong>
+{#if streets?.length === 1}
+	Alle addressen met een gedenkposter in straat <strong><span class="highlight">{streets[0]}</span></strong>
+{:else if streets?.length > 1}
+	Alle addressen met een gedenkposter gefiltered op <strong><span class="highlight">straat</span></strong>
 {:else}
-	Alle addressen met een gedenkposter 
+	Alle addressen met een gedenkposter
 {/if}
 <span class="length">({length})</span>
 </h2>
@@ -27,7 +21,7 @@
 		font-weight: var(--font-weight-light);
 	}
 	.highlight {
-		color: var(--blue-600);
+		color: var(--red-600);
 	}
 
 	span.length {
