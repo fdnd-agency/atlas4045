d19d312f28e99956c0712041aa914dae735f758e
Robin <robin.django@gmail.com>
2025-04-28 13:28:17 +0200

    feat: Added automatic search submission 🚸

--- src/lib/molecules/FilterSearchbar.svelte
+++ src/lib/molecules/FilterSearchbar.svelte
@@ -1,15 +1,36 @@
-
 <script>
   import TextInput from '$lib/atoms/TextInput.svelte';
   import Button from '$lib/atoms/Button.svelte';
   import Checkbox from '$lib/atoms/Checkbox.svelte';
+  import { onMount } from 'svelte';
 
-  let { title, onchange, name } = $props();
+  let { title, onevent, name, formRef} = $props();
+
+  let searchTimeout;
+  let inputRef;
+  
+  function debounceSearch(event) {
+    // Clear any existing timeout
+    clearTimeout(searchTimeout);
+    
+    // Set a new timeout to submit the form after 300ms of no typing
+    searchTimeout = setTimeout(() => {
+        inputRef.form.requestSubmit();
+    }, 600);
+  }
 </script>
 
 <fieldset>
   <legend>Zoeken</legend>
-  <TextInput id="search" placeholder="Zoek op naam, adres, huisnummer..." sronly={true} name="" inputClass={$css('input')}>Zoeken</TextInput>
+  <TextInput 
+    id="search" 
+    placeholder="Zoek op naam, adres, huisnummer..." 
+    sronly={true} 
+    name="q" 
+    inputClass={$css('input')}
+    bind:inputRef
+    onkeyup={debounceSearch}
+  >Zoeken</TextInput>
 </fieldset>
 
 
