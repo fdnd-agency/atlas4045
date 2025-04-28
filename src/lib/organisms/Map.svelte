0ed5d6507ef684a4d0c236ab4b4159d6b8ba93a8
Robin <robin.django@gmail.com>
2025-04-28 16:00:08 +0200

    feat: Added explicit width and height to marker popup img :zap:

--- src/lib/organisms/Map.svelte
+++ src/lib/organisms/Map.svelte
@@ -34,7 +34,7 @@
     mapAddresses.forEach((marker) => {
       const popup = new L.popup({});
 
-      popup.setContent(`<div style="width: 100px;"><p><strong>${marker.street}</strong> ${marker.house_number} ${marker.floor ?? ''} ${marker.addition ?? ''}</p><img style="width: 100%; margin-bottom: 0.75rem;" src="https://fdnd-agency.directus.app/assets/${marker.poster.covers[0].directus_files_id.id}" alt="Afbeelding van ${marker.street} ${marker.house_number} ${marker.floor ?? ''} ${marker.addition ?? ''}"><a href="/adressen/${marker.id}" data-sveltekit-reload>Bekijk poster</a></div>`);
+      popup.setContent(`<div style="width: 100px;"><p><strong>${marker.street}</strong> ${marker.house_number} ${marker.floor ?? ''} ${marker.addition ?? ''}</p><img width="${marker.poster.covers[0].directus_files_id.width}" height="${marker.poster.covers[0].directus_files_id.height}" style="width: 100%; height: auto; margin-bottom: 0.75rem;" src="https://fdnd-agency.directus.app/assets/${marker.poster.covers[0].directus_files_id.id}" alt="Afbeelding van ${marker.street} ${marker.house_number} ${marker.floor ?? ''} ${marker.addition ?? ''}"><a href="/adressen/${marker.id}" data-sveltekit-reload>Bekijk poster</a></div>`);
       
       const customIcon = leaflet.icon({
         iconUrl: (activeMapAddresses ? '/assets/icons/marker-inactive.svg' : '/assets/icons/marker.svg'),
@@ -54,7 +54,7 @@
     activeMapAddresses.forEach((marker) => {
       const popup = new L.popup({});
 
-      popup.setContent(`<div style="width: 100px;"><p><strong>${marker.street}</strong> ${marker.house_number} ${marker.floor ?? ''} ${marker.addition ?? ''}</p><img style="width: 100%; margin-bottom: 0.75rem;" src="https://fdnd-agency.directus.app/assets/${marker.poster.covers[0].directus_files_id.id}" alt="Afbeelding van ${marker.street} ${marker.house_number} ${marker.floor ?? ''} ${marker.addition ?? ''}"><a href="/adressen/${marker.id}" data-sveltekit-reload>Bekijk poster</a></div>`);
+      popup.setContent(`<div style="width: 100px;"><p><strong>${marker.street}</strong> ${marker.house_number} ${marker.floor ?? ''} ${marker.addition ?? ''}</p><img width="${marker.poster.covers[0].directus_files_id.width}" height="${marker.poster.covers[0].directus_files_id.height}" style="width: 100%; height: auto; margin-bottom: 0.75rem;" src="https://fdnd-agency.directus.app/assets/${marker.poster.covers[0].directus_files_id.id}" alt="Afbeelding van ${marker.street} ${marker.house_number} ${marker.floor ?? ''} ${marker.addition ?? ''}"><a href="/adressen/${marker.id}" data-sveltekit-reload>Bekijk poster</a></div>`);
       
       const customIcon = leaflet.icon({
         iconUrl: '/assets/icons/marker-active.svg',
