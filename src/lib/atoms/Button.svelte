04c1f4016018636c99395efd957b743fdcdf46d4
Robin <robin.django@gmail.com>
2025-05-19 10:36:45 +0200

    refactor: Removed dead code from Button ⚰️

--- src/lib/atoms/Button.svelte
+++ src/lib/atoms/Button.svelte
@@ -1,42 +1,51 @@
 <script>
-	let { children, href = null, type = null, onclick = null, class: classList = "", buttonClass = "", ...rest} = $props();
+	let {
+		children,
+		href = null,
+		type = null,
+		onclick = null,
+		class: classList = '',
+		buttonClass = '',
+		...rest
+	} = $props();
 </script>
 
 <!-- Renders <a> styled as button if a href is given -->
 {#if href}
-	<a {...rest} {href} class={["no-focus", classList, buttonClass]}>
+	<a {...rest} {href} class={['no-focus', classList, buttonClass]}>
 		{@render children()}
 	</a>
-<!-- Renders <button> with onclick event if onclick function is given -->
-{:else if typeof onclick === "function" || type === "submit"}
-	<button {...rest} {onclick} class={["no-focus", classList, buttonClass]}>
+	<!-- Renders <button> with onclick event if onclick function is given -->
+{:else if typeof onclick === 'function' || type === 'submit'}
+	<button {...rest} {onclick} class={['no-focus', classList, buttonClass]}>
 		{@render children()}
 	</button>
-<!-- Renders dummy <button> without onclick if neither is given -->
+	<!-- Renders dummy <button> without onclick if neither is given -->
 {:else}
-  <button {...rest} class={["no-focus", classList, buttonClass]}>
-    {@render children()}
-  </button>
+	<button {...rest} class={['no-focus', classList, buttonClass]}>
+		{@render children()}
+	</button>
 {/if}
 
 <style>
-	a, button {
-    font-family: inherit;
+	a,
+	button {
+		font-family: inherit;
 		display: inline-block;
 		width: fit-content;
 		height: fit-content;
 		text-align: center;
 		color: var(--neutral-900);
-    cursor: pointer;
-    font-size: 1rem;
-		background:transparent;
-		text-decoration:none;
-		padding:0;
+		cursor: pointer;
+		font-size: 1rem;
+		background: transparent;
+		text-decoration: none;
+		padding: 0;
 
 		transition-property: color;
 		transition-duration: 0.15s;
 		transition-timing-function: ease-in-out;
-		border:none;
+		border: none;
 
 		&:hover,
 		&:focus,
@@ -49,20 +58,16 @@
 	.highlight {
 		border-radius: var(--border-radius-pill);
 		background-color: var(--blue-500);
-		color:#fff;
+		color: #fff;
 		padding: var(--spacing-xs) var(--spacing-sm);
-		box-shadow:1px 1px 5px rgb(0,0,0,.2);
+		box-shadow: 1px 1px 5px rgb(0, 0, 0, 0.2);
 
 		&:hover,
 		&:focus,
-		&:focus-visible  {
-			color:#fff;
+		&:focus-visible {
+			color: #fff;
 			background-color: var(--blue-700);
 			text-decoration: none;
 		}
-	}
-
-	.logout {
-		font-size: .9rem;
 	}
 </style>
