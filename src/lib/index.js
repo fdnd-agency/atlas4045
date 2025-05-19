cb7bdf7c44ae280e397250c5af29abcb9c7059a3
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-19 12:17:16 +0200

    feat: added index.js
    Put every component in the index.js.
    Divided with the atomic design

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,25 @@
-// place files you want to import through the `$lib` alias in this folder.
+// ATOMS
+export { default as Button } from '$lib/atoms/Button.svelte';
+export { default as Checkbox } from '$lib/atoms/Checkbox.svelte';
+export { default as DirectusImage } from '$lib/atoms/DirectusImage.svelte';
+export { default as FilterCard } from '$lib/atoms/FilterCard.svelte';
+export { default as FilterChip } from '$lib/atoms/FilterChip.svelte';
+export { default as Loader } from '$lib/atoms/Loader.svelte';
+export { default as NavItem } from '$lib/atoms/NavItem.svelte';
+export { default as PostersTitle } from '$lib/atoms/PostersTitle.svelte';
+export { default as TextInput } from '$lib/atoms/TextInput.svelte';
+
+// MOLECULES
+export { default as ActiveFilters } from '$lib/molecules/ActiveFilters.svelte';
+export { default as FilterSearchbar } from '$lib/molecules/FilterSearchbar.svelte';
+export { default as FilterSectionList } from '$lib/molecules/FilterSectionList.svelte';
+export { default as FilterSectionSearch } from '$lib/molecules/FilterSectionSearch.svelte';
+export { default as Nav } from '$lib/molecules/Nav.svelte';
+export { default as PosterCard } from '$lib/molecules/PosterCard.svelte';
+
+// ORGANISMS
+export { default as Header } from '$lib/organisms/Header.svelte';
+export { default as Map } from '$lib/organisms/Map.svelte';
+export { default as PosterCarousel } from '$lib/organisms/PosterCarousel.svelte';
+export { default as PosterOverview } from '$lib/organisms/PosterOverview.svelte';
+export { default as SideFilter } from '$lib/organisms/SideFilter.svelte';
