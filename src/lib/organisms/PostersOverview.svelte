ede1b4310fa91a8a8886f184039d8057a7c9b2d5
Robin <robin.django@gmail.com>
2025-05-20 10:25:01 +0200

    refactor: Deleted non-working loading state in PostersOverview ⚰️

--- src/lib/organisms/PostersOverview.svelte
+++ src/lib/organisms/PostersOverview.svelte
@@ -1,13 +1,12 @@
 <script>
 	import PosterCard from '../molecules/PosterCard.svelte';
 	import OverviewTitle from '$lib/molecules/OverviewTitle.svelte';
-	import Button from '$lib/atoms/Button.svelte';
 
 	import { flip } from 'svelte/animate';
 	import { fade } from 'svelte/transition';
 	import { cubicOut } from 'svelte/easing';
 
-	let { addresses = []} = $props();
+	let { addresses = [] } = $props();
 </script>
 
 <section class="overview">
@@ -15,36 +14,30 @@
 		<OverviewTitle length={addresses.length} />
 	</header>
 
-	{#await addresses}
-		<p>Loading posters...</p>
-	{:then addresses}
-		{#if addresses.length > 0}
-			<ul>
-				{#each addresses as address (address.id)}
-					<div
-						transition:fade={{ duration: 300, easing: cubicOut }}
-						animate:flip={{ duration: 300, easing: cubicOut }}
-					>
-						<PosterCard
-							name={address.person?.[0]?.last_name ?? 'Onbekend'}
-							street={address.street ?? 'Onbekend'}
-							house_number={address.house_number ?? 'Onbekend'}
-							floor={address?.floor}
-							addition={address?.addition}
-							image={address.poster?.covers?.[0]?.directus_files_id?.id ?? ''}
-							width={address.poster?.covers?.[0]?.directus_files_id?.width ?? 419}
-							height={address.poster?.covers?.[0]?.directus_files_id?.height ?? 585}
-							id={address?.id ?? ''}
-						/>
-					</div>
-				{/each}
-			</ul>
-		{:else}
-			<p>Geen posters gevonden. Probeer de filters te wijzigen, of probeer het later opnieuw.</p>
-		{/if}
-	{:catch error}
-		<p>error loading posters: {error.message}</p>
-	{/await}
+	{#if addresses.length > 0}
+		<ul>
+			{#each addresses as address (address.id)}
+				<div
+					transition:fade={{ duration: 300, easing: cubicOut }}
+					animate:flip={{ duration: 300, easing: cubicOut }}
+				>
+					<PosterCard
+						name={address.person?.[0]?.last_name ?? 'Onbekend'}
+						street={address.street ?? 'Onbekend'}
+						house_number={address.house_number ?? 'Onbekend'}
+						floor={address?.floor}
+						addition={address?.addition}
+						image={address.poster?.covers?.[0]?.directus_files_id?.id ?? ''}
+						width={address.poster?.covers?.[0]?.directus_files_id?.width ?? 419}
+						height={address.poster?.covers?.[0]?.directus_files_id?.height ?? 585}
+						id={address?.id ?? ''}
+					/>
+				</div>
+			{/each}
+		</ul>
+	{:else}
+		<p>Geen posters gevonden. Probeer de filters te wijzigen, of probeer het later opnieuw.</p>
+	{/if}
 </section>
 
 <style>
@@ -55,7 +48,7 @@
 		align-items: center;
 		padding: var(--spacing-md) var(--spacing-xs) 0;
 	}
-  
+
 	ul {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
@@ -67,9 +60,9 @@
 		padding-top: var(--spacing-lg);
 	}
 
-  section.overview {
-    padding: 0 var(--page-padding);
-  }
+	section.overview {
+		padding: 0 var(--page-padding);
+	}
 
 	@media screen and (min-width: 800px) {
 		div:global(:has(.landscape)) {
@@ -87,8 +80,8 @@
 			padding-top: var(--spacing-md);
 		}
 
-    section.overview {
-      padding: 0 var(--spacing-md);
-    }
+		section.overview {
+			padding: 0 var(--spacing-md);
+		}
 	}
 </style>
